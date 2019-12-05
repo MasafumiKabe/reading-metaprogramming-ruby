f2705c8b8995ecbc92b46cf243aafee84ac5bf02
MasafumiKabe <msfmkb@gmail.com>
2019-12-05 14:40:12 +0900

    問題といた

--- 02_object_model/hoge.rb
+++ 02_object_model/hoge.rb
@@ -4,4 +4,13 @@ class Hoge
   # "hoge" という文字列を返すhogehogeメソッドを持つ
   # HogeクラスのスーパークラスはStringである
   # 自身が"hoge"という文字列である時（HogeクラスはStringがスーパークラスなので、当然自身は文字列である）、trueを返すhoge?メソッドが定義差されている
+  Hoge = "hoge"
+
+  def hogehoge
+    "hoge"
+  end
+
+  def hoge?
+    self == "hoge"
+  end
 end
